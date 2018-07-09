1b1bde18765658769e60b047783d91006191a6fb
Nadrieril <nadrieril@gmail.com>
2018-07-09 15:28:29 +0100

    Cleaner parsing

--- make-layout-json.py
+++ make-layout-json.py
@@ -3,7 +3,7 @@
 
 import sys, re, json
 
-xkb_line_regex = re.compile("^ key (?P<key><\w+>) { \[\], \[ (\w+), (\w+), (\w+), (\w+) \] };$".replace(" ", "\s*"))
+xkb_line_regex = re.compile("^ key (?P<key><\w+>) { \[\], \[(.+)\] };$".replace(" ", "\s*"))
 finger_colors = {
      0: "#474747",
      1: "#7d1b22",
@@ -38,18 +38,23 @@ with open("fingers-wide.json") as f:
         layout = layout.replace("\"%s\"" % (key,), "{\"c\":\"%s\"},\"%s\"" %
                 (finger_colors[f], key))
 
+key_map = {}
 with open("custom.xkb") as f:
     for row in f:
         if row == "": continue
         r = re.match(xkb_line_regex, row)
         if r is None: continue
         key = r.group("key")
-        symbs = [ pretty(r.group(i)) for i in range(2, 5) ]
-        if symbs[1] == symbs[0].upper():
-            symbs[0] = symbs[1]
-            symbs[1] = ""
-        symb_str = "%s\\n\\n\\n%s\\n\\n\\n\\n\\n\\n%s" % (symbs[1], symbs[2], symbs[0])
-        layout = layout.replace(key, symb_str)
+        key_map[key] = r.group(2)
+
+for (key, symbs) in key_map.items():
+    symbs = [ pretty(x.strip()) for x in symbs.split(",") ]
+    if symbs[1] == symbs[0].upper():
+        symbs[0] = symbs[1]
+        symbs[1] = ""
+    symb_str = "%s\\n\\n\\n%s\\n\\n\\n\\n\\n\\n%s" % (symbs[1], symbs[2], symbs[0])
+    layout = layout.replace(key, symb_str)
+
 
 print(layout)
 
