5202250ea42a510328d3a7e26db5e545361ad59a
Nadrieril <nadrieril@gmail.com>
2018-07-09 21:24:32 +0100

    geenrate xkb file instead of pasring it

--- make-layout-json.py
+++ make-layout-json.py
@@ -3,7 +3,22 @@
 
 import sys, re, json
 
-xkb_line_regex = re.compile("^ key (?P<key><\w+>) { \[\], \[(.+)\] };$".replace(" ", "\s*"))
+FINGER_MAP_FILE = "fingers-wide.json"
+LAYOUT_NAME = "Colemak (wide mod, DH mod, angle mod, custom modifications)"
+XKB_TEMPLATE = """
+xkb_keymap {
+    xkb_keycodes  { include "evdev+aliases(azerty)" };
+    xkb_types     { include "complete" };
+    xkb_compat    { include "complete" };
+    xkb_symbols   {
+        include "pc+fr+inet(evdev)+group(shifts_toggle)+ctrl(nocaps)+compose(paus)"
+        name[Group2]= "%s";
+%s
+        include "level3(ralt_switch)"
+    };
+    xkb_geometry  { include "pc(pc105)" };
+};
+"""
 finger_colors = {
      0: "#474747",
      1: "#7d1b22",
@@ -29,23 +44,35 @@ def pretty(x):
 with open("symbols.json") as f:
     symbol_dict = json.loads(f.read())
 
-with open("layout-template.json") as f:
-    layout = f.read()
 
-with open("fingers-wide.json") as f:
-    fingers = json.loads(f.read())
-    for key, f in fingers.items():
-        layout = layout.replace("\"%s\"" % (key,), "{\"c\":\"%s\"},\"%s\"" %
-                (finger_colors[f], key))
+with open("layout-order.json") as f:
+    layout_order = json.loads(f.read())
+
+with open("layout-map.json") as f:
+    layout_map = json.loads(f.read())
 
 key_map = {}
-with open("custom.xkb") as f:
-    for row in f:
-        if row == "": continue
-        r = re.match(xkb_line_regex, row)
-        if r is None: continue
-        key = r.group("key")
-        key_map[key] = r.group(2)
+for (x, y) in zip(layout_order, layout_map):
+    for (k, v) in zip(x, y):
+        key_map[k] = v
+
+
+with open("custom.xkb", "w") as f:
+    key_defs = "\n".join(
+        "        key %s { [], [ %s ] };" % (k, v)
+        for (k, v) in key_map.items()
+    )
+    f.write(XKB_TEMPLATE % (LAYOUT_NAME, key_defs))
+
+
+with open("layout-template.json") as f:
+    layout_template = f.read()
+
+with open(FINGER_MAP_FILE) as f:
+    fingers = json.loads(f.read())
+    for key, f in fingers.items():
+        layout_template = layout_template.replace("\"%s\"" % (key,), "{\"c\":\"%s\"},\"%s\"" %
+                (finger_colors[f], key))
 
 for (key, symbs) in key_map.items():
     symbs = [ pretty(x.strip()) for x in symbs.split(",") ]
@@ -53,8 +80,8 @@ for (key, symbs) in key_map.items():
         symbs[0] = symbs[1]
         symbs[1] = ""
     symb_str = "%s\\n\\n\\n%s\\n\\n\\n\\n\\n\\n%s" % (symbs[1], symbs[2], symbs[0])
-    layout = layout.replace(key, symb_str)
+    layout_template = layout_template.replace(key, symb_str)
 
 
-print(layout)
+print(layout_template)
 
