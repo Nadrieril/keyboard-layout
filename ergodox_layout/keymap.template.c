046422bec1f0cc905dbe3dde6d79657aa3ae702c
Nadrieril <nadrieril@gmail.com>
2019-03-12 16:51:02 +0100

    Add custom swap_hands layer

--- ergodox_layout/keymap.template.c
+++ ergodox_layout/keymap.template.c
@@ -13,43 +13,21 @@ __KEYMAP_GOES_HERE__
 };
 
 uint32_t layer_state_set_user(uint32_t state) {
-
-    uint8_t layer = biton32(state);
-
     ergodox_board_led_off();
     ergodox_right_led_1_off();
     ergodox_right_led_2_off();
     ergodox_right_led_3_off();
-    switch (layer) {
-      case 1:
+    if (state & (1<<1)) {
         ergodox_right_led_1_on();
-        break;
-      case 2:
+    }
+    if (state & (1<<2)) {
         ergodox_right_led_2_on();
-        break;
-      case 3:
+    }
+    if (state & (1<<3)) {
         ergodox_right_led_3_on();
-        break;
-      case 4:
-        ergodox_right_led_1_on();
-        ergodox_right_led_2_on();
-        break;
-      case 5:
-        ergodox_right_led_1_on();
-        ergodox_right_led_3_on();
-        break;
-      case 6:
-        ergodox_right_led_2_on();
-        ergodox_right_led_3_on();
-        break;
-      case 7:
-        ergodox_right_led_1_on();
-        ergodox_right_led_2_on();
-        ergodox_right_led_3_on();
-        break;
-      default:
-        break;
+        swap_hands = true;
+    } else {
+        swap_hands = false;
     }
     return state;
-
 };
