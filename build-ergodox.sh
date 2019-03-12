09fec6025d062b9ab719b2d8d1f24c3dfd52520d
Nadrieril <nadrieril@gmail.com>
2019-03-12 14:52:56 +0100

    Generate layout from qmk json file

--- build-ergodox.sh
+++ build-ergodox.sh
@@ -2,5 +2,13 @@
 #!nix-shell -i bash /home/nadrieril/wip/forks/qmk_firmware/shell.nix
 set -e
 
+cat ergodox_layout/layers.json \
+    | jq -r '.layers | to_entries[] | "  [\(.key)] = LAYOUT_ergodox(\(.value | join(","))),"' \
+    > ergodox_layout/.contents
+cat ergodox_layout/keymap.template.c \
+    | sed -e '/__KEYMAP_GOES_HERE__/r ergodox_layout/.contents' \
+          -e '/__KEYMAP_GOES_HERE__/d' \
+    > ergodox_layout/keymap.c
+rm ergodox_layout/.contents
 cd $HOME/wip/forks/qmk_firmware
 make ergodox_ez:nadrieril:teensy
