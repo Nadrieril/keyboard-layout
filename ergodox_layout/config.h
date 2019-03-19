60a2a0139b7d292cdd21b19272cf63c8fad711ac
Nadrieril <nadrieril@gmail.com>
2019-03-19 17:29:50 +0100

    Ergodox tweaks

--- ergodox_layout/config.h
+++ ergodox_layout/config.h
@@ -2,5 +2,12 @@
   Set any config.h overrides for your specific keymap here.
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+#define QMK_KEYS_PER_SCAN 4
 #undef IGNORE_MOD_TAP_INTERRUPT
+#define PERMISSIVE_HOLD
+// #undef TAPPING_TERM
+// #define TAPPING_TERM 50
+// #define RETRO_TAPPING
 
