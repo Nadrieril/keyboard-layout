8d282f51eadab23a65db4544d40b78d1ee7809a2
Nadrieril <nadrieril+git@gmail.com>
2019-12-13 11:10:01 +0000

    Tweak

--- extract_keylogger_stats.sh
+++ extract_keylogger_stats.sh
@@ -3,6 +3,7 @@ cat ~/.dotfiles/keys.log \
     | grep KeyPress \
     | ~/.dotfiles/shell/bin/f 4 3 \
     | sed -e 's/Num_Lock//' \
+    | sed -e 's/ISO_Level3_Shift/Alt_Gr/' \
     | sed -e 's/\[,/[/' -e 's/,\]/]/' -e 's/,,/,/' \
     | sed -e 's/,/ + /g' -e 's/\[\] //' -e 's/\] \[/ + /' -e 's/\[//g' -e 's/\]//g' \
     | sort \
