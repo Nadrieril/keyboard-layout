71b5ca358c21318cdb0a2213b0dd23301b3a70ba
Nadrieril <nadrieril+git@gmail.com>
2024-10-23 23:27:13 +0200

    Add swap-hands to voyager!

--- voyager/keymap.c
+++ voyager/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_A,           KC_Z,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSPC,
     KC_LEFT_CTRL,   KC_Q,           KC_S,           KC_D,           KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_M,           KC_ENTER,
     KC_LEFT_SHIFT,  MT(MOD_LALT, KC_W),KC_X,           MT(MOD_LGUI, KC_C),MT(MOD_LALT, KC_V),MT(MOD_LALT, KC_B),                                MT(MOD_RALT, KC_N),MT(MOD_RALT, KC_COMMA),KC_SCLN,        LSFT(KC_SCLN),  MT(MOD_RALT, KC_DOT),KC_BSPC,
-                                                    LT(1,KC_SPACE), KC_TRANSPARENT,                                 MO(2),          KC_LEFT_SHIFT
+                                                    LT(1,KC_SPACE), QK_SWAP_HANDS_MOMENTARY_ON,                                 MO(2),          KC_LEFT_SHIFT
   ),
   [1] = LAYOUT_voyager(
     KC_AUDIO_VOL_UP,KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,                                          KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
