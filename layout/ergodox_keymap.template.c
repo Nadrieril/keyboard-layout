c9fe6706684c62627fd31e29903dd8c601c59a78
Nadrieril <nadrieril+git@gmail.com>
2025-06-12 21:14:16 +0200

    Make qwerty layout and cycle toggle key

--- layout/ergodox_keymap.template.c
+++ layout/ergodox_keymap.template.c
@@ -6,7 +6,7 @@
 // 13: super
 // 11: alt-gr
 // 10: shift
-// 8: MIDI (see tutorial: https://sndrtj.eu/2019/10/19/Using-a-USB-midi-keyboard-on-Ubuntu/, also http://tedfelix.com/linux/linux-midi.html)
+// 8: QWERTY
 // 7: windows compat + gaming layer
 // 6: symbols layer
 // 5: numpad layer
@@ -20,7 +20,7 @@
 #define FN_LAYER_ID 5
 #define SYMBOLS_LAYER_ID 6
 #define GAME_LAYER_ID 7
-#define MIDI_LAYER_ID 8
+#define QWERTY_LAYER_ID 8
 #define SHIFT_LAYER_ID 10
 #define RALT_LAYER_ID 11
 #define SUPER_LAYER_ID 13
@@ -45,14 +45,15 @@ layer_state_t layer_state_set_user(layer_state_t state) {
         ergodox_right_led_1_set(10);
     }
     if (is_layer_on(state, ALT_LAYOUT_LAYER_ID)
-            || is_layer_on(state, SUDOKU_LAYER_ID)
-            || is_layer_on(state, SUDOKU_LETTERS_LAYER_ID)
+            || is_layer_on(state, QWERTY_LAYER_ID)
             || is_layer_on(state, GAME_LAYER_ID)) {
         ergodox_right_led_2_on();
         ergodox_right_led_2_set(10);
     }
     if (is_layer_on(state, SYMBOLS_LAYER_ID)
-            || is_layer_on(state, MIDI_LAYER_ID)) {
+            || is_layer_on(state, QWERTY_LAYER_ID)
+            || is_layer_on(state, SUDOKU_LAYER_ID)
+            || is_layer_on(state, SUDOKU_LETTERS_LAYER_ID)) {
         ergodox_right_led_3_on();
         ergodox_right_led_3_set(10);
     }
